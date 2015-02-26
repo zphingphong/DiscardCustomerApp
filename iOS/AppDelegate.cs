3ab6b8ea7158665f3cc284eb6656a8096f02184a
Pan K <paninee@khantidhara.com>
2015-02-25 17:23:24 -0800

    Add Facebook and Google icon to button and make the button text even.

--- iOS/AppDelegate.cs
+++ iOS/AppDelegate.cs
@@ -10,6 +10,7 @@ namespace com.panik.discard.ios {
 	public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate {
 		public override bool FinishedLaunching (UIApplication app, NSDictionary options) {
 			global::Xamarin.Forms.Forms.Init ();
+			UINavigationBar.Appearance.BarTintColor = UIColor.White;
 
 			LoadApplication (App.instance);
 
